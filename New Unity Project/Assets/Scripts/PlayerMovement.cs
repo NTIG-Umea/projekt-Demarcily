4092fbfce1fae5c43466faf1b72225d1d461bda9
Demarcily <60388907+Demarcily@users.noreply.github.com>
2020-05-05 14:49:19 +0200

    Code för movement

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(Horizontal, 0.0f, Vertical);
+        rb.AddForce(movement * speed);
+    }
+}
